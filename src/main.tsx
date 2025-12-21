abd3722701946dbab156d17775104e64aa9a2458
altanis7 <al_tanis@naver.com>
2025-12-21 14:14:03 +0900

    feat: MSW 초기화 로직 추가
    환경 변수 기반으로 MSW 활성화 여부 결정
    개발 환경에서 Mock API 자동 시작

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,20 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./globals.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+// MSW 초기화
+async function enableMocking() {
+  if (import.meta.env.VITE_USE_MSW === "true") {
+    const { startMockServiceWorker } = await import("./mocks/browser");
+    await startMockServiceWorker();
+  }
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
+  );
+});
