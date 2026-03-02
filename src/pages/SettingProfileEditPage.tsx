792d708a21a58ca1284f4d2eed149621754528cf
altanis7 <al_tanis@naver.com>
2026-03-02 20:19:57 +0900

    feat: 프로필 편집 페이지 추가
    - 기존 MyInfoForm 컴포넌트 재사용
    - /setting/profile 라우트로 접근

---
+++ src/pages/SettingProfileEditPage.tsx
@@ -0,0 +1,10 @@
+import RootLayout from "@/shared/components/layouts/RootLayout";
+import { MyInfoForm } from "@/features/member/components/MyInfoForm";
+
+export function SettingProfileEditPage() {
+  return (
+    <RootLayout>
+      <MyInfoForm />
+    </RootLayout>
+  );
+}
